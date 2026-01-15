6e0347b290cc313ab4f43260e6cd881c9237960f
ozzyyes <oguzhan.yesildag.10@gmail.com>
2026-01-15 18:36:17 -0500

    Made changes on lesson02 package

--- src/com/neotech/lesson2/BrowserCommands.java
+++ src/com/neotech/lesson2/BrowserCommands.java
@@ -1,46 +1,13 @@
 package com.neotech.lesson2;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserCommands {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		WebDriver driver = new ChromeDriver();
-		
-		//driver.get("https://www.zillow.com/");
 		
 		
-		driver.navigate().to("https://www.zillow.com/"); //will maintain the browsing history
-		
-		Thread.sleep(4000);
-		
-		//maximize the window
-		driver.manage().window().maximize();
-		//driver.manage().window().fullscreen();  2nd way
-		
-		Thread.sleep(3000);
-		
-		
-		driver.navigate().to("https://www.google.com/");
-		
-		Thread.sleep(3000);
-		
-		driver.navigate().back();
-		
-		driver.navigate().refresh();
-		
-		Thread.sleep(3000);
-		
-		String url = driver.getCurrentUrl();
-		if(url.contains("zillow")) {
-			System.out.println("Test passed!");
-		} else {
-			System.out.println("Test failed!");
-			System.out.println("Current url: " +url);
-		}
-		
+
 		
 		
 	}
